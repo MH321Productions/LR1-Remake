53a9cc36fdc38c1d46bb91dac2c9d580a629cf15
Mika Herrmann <mika.herrmann@js-soft.com>
2026-07-27 16:09:04 +0200

    Add basic initialization and cleanup of window and mixer

--- include/LR1-remake/app.hpp
+++ include/LR1-remake/app.hpp
@@ -1,6 +1,8 @@
 #ifndef LR1_REMAKE_APP_HPP
 #define LR1_REMAKE_APP_HPP
 
+#include <LR1-remake/util/log.hpp>
+
 struct SDL_Window;
 struct MIX_Mixer;
 union SDL_Event;
@@ -9,6 +11,8 @@ namespace LR1_Remake {
     class Main {
         public:
             Main() : window(nullptr), mixer(nullptr), running(false) {}
+
+            Logger log;
 
             int onExecute();
             bool triggerQuit();
--- src/app/app.cpp
+++ src/app/app.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <SDL3/SDL.h>
+#include <SDL3_mixer/SDL_mixer.h>
 
 #include <LR1-remake/app.hpp>
 
@@ -33,10 +34,34 @@ namespace LR1_Remake {
     }
 
     bool Main::onInit() {
+        if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS | SDL_INIT_GAMEPAD)) {
+            log.fatal << "Couldn't init SDL: " << SDL_GetError() << endl;
+            return false;
+        }
+
+        if (!MIX_Init()) {
+            log.fatal << "Couldn't init SDL_mixer: " << SDL_GetError() << endl;
+            return false;
+        }
+
+        window = SDL_CreateWindow("LR1-Remake", 1280, 720, SDL_WINDOW_VULKAN);
+        if (!window) {
+            log.fatal << "Couldn't create window: " << SDL_GetError() << endl;
+            return false;
+        }
+        log.window = window;
+
+        mixer = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, nullptr);
+        if (!mixer) {
+            log.fatal << "Couldn't create mixer: " << SDL_GetError() << endl;
+            return false;
+        }
+
         return true;
     }
 
     void Main::onCleanup() {
+        if (mixer) MIX_DestroyMixer(mixer);
         if (window) SDL_DestroyWindow(window);
     }
 }
